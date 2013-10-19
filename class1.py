6c87f74d260f6b674f64f2002d624023abda3739
alex <alextooter@gmail.com>
2013-10-20 00:45:04 +0800

    coding

--- class1.py
+++ class1.py
@@ -1,4 +1,38 @@
 __author__ = 'Administrator'
 
-print 3 > '4'
-print 2 * 'alex'
+# this is for class two.
+x = 15
+if (x / 2) * 2 == x:
+    print 'Even'
+else:
+    print 'Odd'
+
+a = 14
+b = 11
+c = 8
+
+if a > b:
+    if b > c:
+        print 'c is smallest'
+    else:
+        print 'b is smallest'
+else:
+    if a > c:
+        print 'c is smallest'
+    else:
+        print 'a is smallest'
+
+
+if a<b and a < c:print 'a is least'
+elif b<c: print 'b is least'
+else: print 'c is least'
+
+
+y = 0
+x = 3
+
+intersLeft = x
+while(intersLeft > 0):
+    y += x
+    intersLeft -= 1
+print y
